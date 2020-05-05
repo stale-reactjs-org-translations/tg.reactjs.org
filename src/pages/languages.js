0a499f96b9ebac58f11f550463759ecd744091d8
ZafarHamidov <zafarjon009@mail.ru>
2020-05-05 12:24:51 +0300

    feat: start of tajik language translation header and footer

--- src/pages/languages.js
+++ src/pages/languages.js
@@ -47,7 +47,7 @@ const Languages = ({location}: Props) => (
     <Container>
       <div css={sharedStyles.articleLayout.container}>
         <div css={sharedStyles.articleLayout.content}>
-          <Header>Languages</Header>
+          <Header>Забонҳо</Header>
           <TitleAndMetaTags
             canonicalUrl={`${urlRoot}/languages/`}
             title="React - Languages"
